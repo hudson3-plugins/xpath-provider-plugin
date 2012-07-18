a20684cec57ae03d8566742644058087290656e7
Bob Foster <bobfoster@gmail.com>
2012-07-18 11:20:21 -0700

    Fix copyright notice

--- src/main/java/org/hudsonci/xpath/impl/StylesheetJaxenImpl.java
+++ src/main/java/org/hudsonci/xpath/impl/StylesheetJaxenImpl.java
@@ -1,7 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*******************************************************************************
+ *
+ * Copyright (c) 2012 Oracle Corporation.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: 
+ *
+ *    Bob Foster
+ *     
+ *******************************************************************************/ 
 
 package org.hudsonci.xpath.impl;
 
